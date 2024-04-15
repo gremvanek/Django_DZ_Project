3108ba0ae79b21fdd51888d5bb83160cc592a947
gremv <gremvanek@gmail.com>
2024-04-15 15:49:51 +0500

    Добавил команду для создания superuser.

--- dz_django/settings.py
+++ dz_django/settings.py
@@ -132,6 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'users/'
 
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Замените на ваш SMTP-сервер
