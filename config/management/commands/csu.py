bf95bed219be3ecd4ed84cdbe71d32c7d789ea26
gremv <gremvanek@gmail.com>
2024-04-23 11:33:34 +0500

    Правки по flake8 :(

--- config/management/commands/csu.py
+++ config/management/commands/csu.py
@@ -6,7 +6,7 @@ from users.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        user = User.objects.create(
+        User.objects.create(
             email="admin@sky.pro",
             is_staff=True,
             is_superuser=True,
